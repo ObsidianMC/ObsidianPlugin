1b694b3a9113e2e8376b2cd8129e531498b001c1
Ryan de Jonge <naamloosdt@gmail.com>
2022-02-22 23:44:04 +0100

    Updated template plugin (same as sample in main repo, but stripped / modified.)

--- ObsidianPlugin/Plugin.cs
+++ ObsidianPlugin/Plugin.cs
@@ -1,4 +1,16 @@
-﻿using Obsidian.API;
+﻿/**
+ * 
+ * It is STRONGLY recommended to get familiar with both the API documentation and the basic plugin guide.
+ * These can be found here:
+ * https://obsidian-mc.net/articles/plugins.html
+ * https://obsidian-mc.net/api/index.html
+ * 
+ * Please DO take note that Obsidian is still in active development,
+ * meaning both the server and it's API are extremely prone to changes!
+ * 
+ */
+
+using Obsidian.API;
 using Obsidian.API.Events;
 using Obsidian.API.Plugins;
 using Obsidian.API.Plugins.Services;
@@ -7,36 +19,20 @@ using System.Threading.Tasks;
 
 namespace ObsidianPlugin
 {
-    [Plugin(name: "Sample Plugin", Version = "1.0",
-            Authors = "Obsidian Team", Description = "My sample plugin.",
-            ProjectUrl = "https://github.com/Naamloos/Obsidian")]
+    [Plugin(name: "Template Plugin", Version = "1.0",
+            Authors = "Obsidian Team", Description = "My Plugin.",
+            ProjectUrl = "https://github.com/ObsidianMC/Obsidian")]
     public class ObsidianPlugin : PluginBase
     {
         // Any interface from Obsidian.Plugins.Services can be injected into properties
         [Inject] public ILogger Logger { get; set; }
         [Inject] public IFileReader FileReader { get; set; }
 
-        // Dependencies will be injected automatically, if dependency class and field/property names match
-        // Plugins won't load until all their required dependencies are added
-        // Optional dependencies may be injected at any time, if at all
-        [Dependency(minVersion: "2.0", optional: true), Alias(identifier: "Sample Remote Plugin")]
-        public MyWrapper SampleRemotePlugin { get; set; }
-
         // One of server messages, called when an event occurs
         public void OnLoad(IServer server)
         {
-            Logger.Log(message: $"§a{Info.Name} §floaded! Hello §a{server.DefaultWorld.Name}§f!");
+            Logger.Log(message: $"§a{Info.Name} §floaded! Hello §aWorld§f!");
             Logger.Log(message: $"Hello! I live at §a{FileReader.CreateWorkingDirectory()}§f!");
-        }
-
-        public void OnPermissionRevoked(PermissionRevokedEventArgs args)
-        {
-            Logger.Log(message: $"Permission {args.Permission} revoked from player {args.Player.Username}");
-        }
-
-        public void OnPermissionGranted(PermissionGrantedEventArgs args)
-        {
-            Logger.Log(message: $"Permission {args.Permission} granted to player {args.Player.Username}");
         }
 
         public async Task OnPlayerJoin(PlayerJoinEventArgs playerJoinEvent)
@@ -47,7 +43,7 @@ namespace ObsidianPlugin
         }
 
         [Command(commandName: "plugincommand")]
-        [CommandInfo(description: "woop dee doo this command is from within a plugin class!!")]
+        [CommandInfo(description: "woop dee doo this command is from within a plugin's own class!!")]
         public async Task PluginCommandAsync(CommandContext ctx)
         {
             await ctx.Sender.SendMessageAsync(message: "Hello from plugin command implemented in Plugin class!");
@@ -55,7 +51,7 @@ namespace ObsidianPlugin
     }
 
     [CommandRoot]
-    public class MyCommands
+    public class MyCommands // Fun fact: You can actually move this class to a different file and it'll work just fine.
     {
         [Inject]
         public ObsidianPlugin Plugin { get; set; }
@@ -63,22 +59,13 @@ namespace ObsidianPlugin
         [Inject]
         public ILogger Logger { get; set; }
 
-        [Command("mycommand")]
-        [CommandInfo("woop dee doo this command is from a plugin")]
+        [Command("classcommand")]
+        [CommandInfo("woop dee doo this command is from a plugin's command class!!")]
         public async Task MyCommandAsync(CommandContext ctx)
         {
             Plugin.Logger.Log("Testing Plugin as injected dependency");
             Logger.Log("Testing Services as injected dependency");
             await ctx.Player.SendMessageAsync("Hello from plugin command!");
         }
-    }
-
-    public class MyWrapper : PluginWrapper
-    {
-        public Action Step { get; set; }
-        [Alias("get_StepCount")] private Func<int> GetStepCount { get; set; }
-        [Alias("set_StepCount")] private Action<int> SetStepCount { get; set; }
-
-        public int StepCount { get => GetStepCount(); set => SetStepCount(value); }
     }
 }
