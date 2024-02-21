87e6e6179228144228a0232b842925391fabb5ad
Ryan de Jonge <ryandejonge@outlook.com>
2024-02-21 01:37:28 +0100

    Update to latest API nightly

--- ObsidianPlugin/Plugin.cs
+++ ObsidianPlugin/Plugin.cs
@@ -15,41 +15,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32;
 using Obsidian.API;
 using Obsidian.API.Events;
 using Obsidian.API.Plugins;
-using Obsidian.API.Plugins.Services;
 
 namespace ObsidianPlugin
 {
-    [Plugin(name: "Template Plugin", Version = "1.0",
-            Authors = "Obsidian Team", Description = "My Plugin.",
-            ProjectUrl = "https://github.com/ObsidianMC/Obsidian")]
     public class Plugin : PluginBase
     {
         // Any interface from Obsidian.Plugins.Services can be injected into properties
-        [Inject] public ILogger Logger { get; set; }
+        [Inject] public ILogger<Plugin> Logger { get; set; }
+
+        public override void ConfigureRegistry(IPluginRegistry pluginRegistry)
+        {
+            pluginRegistry.MapEvent(OnPlayerJoin);
+            pluginRegistry.MapCommand("plugincommand", PluginCommandAsync);
+        }
 
         // One of server messages, called when an event occurs
-        public void OnLoad(IServer server)
+        public override ValueTask OnLoadAsync(IServer server)
         {
-            Logger.Log(message: $"§a{Info.Name} §floaded! §aHello World§f!");
-            Logger.Log(message: $"Hello! I live at §a{this.GetType().Assembly.Location}§f!");
+            Logger.LogInformation("§a{name} §floaded! §aHello World§f!", Info.Name);
+            Logger.LogInformation("Hello! I live at §a{location}§f!", this.GetType().Assembly.Location);
+            return ValueTask.CompletedTask;
         }
 
         public async Task OnPlayerJoin(PlayerJoinEventArgs playerJoinEvent)
         {
             var player = playerJoinEvent.Player;
-
             await player.SendMessageAsync(message: ChatMessage.Simple(text: $"Welcome {player.Username}!", color: ChatColor.Gold));
         }
 
-        [Command(commandName: "plugincommand")]
         [CommandInfo(description: "Command defined in the plugin's own class.")]
         public async Task PluginCommandAsync(CommandContext ctx)
         {
-            // fun fact: did you know that you're not actually restricted to minecraft's base colors?
             await ctx.Player.SendMessageAsync(
                 new ChatMessage()
                 .AppendColor(ChatColor.BrightGreen)
